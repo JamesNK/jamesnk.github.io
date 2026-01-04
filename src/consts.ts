4273cd5a3e30839e6a7efff5b4180f2f6bdf1223
James Newton-King <james@newtonking.com>
2026-01-04 08:00:15 +0800

    Style tweaks

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,5 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'James Newton-King';
+export const SITE_TITLE_SHORT = 'JamesNK';
 export const SITE_DESCRIPTION = 'James Newton-King\'s personal blog';
