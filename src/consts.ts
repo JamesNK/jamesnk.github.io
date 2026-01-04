213770b381a1cdc91d39fbf6ae68e99b8b80e8e5
James Newton-King <james@newtonking.com>
2026-01-04 14:49:18 +0800

    Improve archive page and remove duplication

--- src/consts.ts
+++ src/consts.ts
@@ -4,3 +4,11 @@
 export const SITE_TITLE = 'James Newton-King';
 export const SITE_TITLE_SHORT = 'JamesNK';
 export const SITE_DESCRIPTION = 'James Newton-King\'s personal blog';
+
+export function getPostUrl(post: { data: { pubDate: Date }; id: string }) {
+	const date = post.data.pubDate;
+	const year = date.getFullYear();
+	const month = String(date.getMonth() + 1).padStart(2, '0');
+	const day = String(date.getDate()).padStart(2, '0');
+	return `/archive/${year}/${month}/${day}/${post.id}/`;
+}
